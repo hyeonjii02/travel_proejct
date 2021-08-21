3422b62487aa8fa7e35e3aa9b2697fca1e83b456
hyeonjii02 <hyeonjiii02@gmail.com>
2021-08-21 14:57:50 +0900

    Initial commit

---
+++ src/main/java/com/project/travel/travel_proejct/TravelProejctApplication.java
@@ -0,0 +1,13 @@
+package com.project.travel.travel_proejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelProejctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelProejctApplication.class, args);
+	}
+
+}
